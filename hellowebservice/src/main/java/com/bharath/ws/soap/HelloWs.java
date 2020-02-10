59f6f6fb8c07c0f942b7f1aaa57c77647d145f2d
sacret2 <mrbartosiak@gmail.com>
2020-02-10 21:33:34 +0100

    service config

--- hellowebservice/src/main/java/com/bharath/ws/soap/HelloWs.java
+++ hellowebservice/src/main/java/com/bharath/ws/soap/HelloWs.java
@@ -1,5 +1,13 @@
 package com.bharath.ws.soap;
 
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
+@WebService
 public class HelloWs {
 
+	@WebMethod
+	public String hello() {
+		return "Hello";
+	}
 }
