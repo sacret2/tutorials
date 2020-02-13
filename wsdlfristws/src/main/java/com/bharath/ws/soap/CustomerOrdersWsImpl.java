e84329db7556eda4efbeec86810c68b2df980ee1
sacret2 <mrbartosiak@gmail.com>
2020-02-13 14:46:38 +0100

    CustomerOrdersWsImpl and WebServiceConfig

--- wsdlfristws/src/main/java/com/bharath/ws/soap/CustomerOrdersWsImpl.java
+++ wsdlfristws/src/main/java/com/bharath/ws/soap/CustomerOrdersWsImpl.java
@@ -1,23 +1,67 @@
 package com.bharath.ws.soap;
 
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import com.bharath.ws.trainings.CreateOrdersRequest;
 import com.bharath.ws.trainings.CreateOrdersResponse;
 import com.bharath.ws.trainings.CustomerOrdersPortType;
 import com.bharath.ws.trainings.GetOrdersRequest;
 import com.bharath.ws.trainings.GetOrdersResponse;
+import com.bharath.ws.trainings.Order;
+import com.bharath.ws.trainings.Product;
 
 public class CustomerOrdersWsImpl implements CustomerOrdersPortType {
 
-	@Override
-	public GetOrdersResponse getOrders(GetOrdersRequest parameters) {
-		// TODO Auto-generated method stub
-		return null;
+	Map<BigInteger, List<Order>> customerOrders = new HashMap<>();
+	int currentId;
+
+	public CustomerOrdersWsImpl() {
+		init();
+	}
+
+	public void init() {
+		List<Order> orders = new ArrayList<>();
+		Order order = new Order();
+		order.setId(BigInteger.valueOf(1));
+
+		Product product = new Product();
+		product.setId("1");
+		product.setDescription("IPhone");
+		product.setQuantity(BigInteger.valueOf(3));
+		order.getProduct().add(product);
+
+		orders.add(order);
+
+		customerOrders.put(BigInteger.valueOf(++currentId), orders);
 	}
 
 	@Override
-	public CreateOrdersResponse createOrders(CreateOrdersRequest parameters) {
-		// TODO Auto-generated method stub
-		return null;
+	public GetOrdersResponse getOrders(GetOrdersRequest request) {
+		BigInteger customerId = request.getCustomerId();
+		List<Order> orders = this.customerOrders.get(customerId);
+
+		GetOrdersResponse response = new GetOrdersResponse();
+		response.getOrder().addAll(orders);
+
+		return response;
+	}
+
+	@Override
+	public CreateOrdersResponse createOrders(CreateOrdersRequest request) {
+		BigInteger customerId = request.getCustomerId();
+		Order order = request.getOrder();
+	
+		List<Order> orders = customerOrders.get(customerId);
+		orders.add(order);
+
+		CreateOrdersResponse response = new CreateOrdersResponse();
+		response.setResult(true);
+		
+		return response;
 	}
 
 }
