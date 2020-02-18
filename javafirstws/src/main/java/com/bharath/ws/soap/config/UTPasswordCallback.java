574910be4e358e8929a290ec8e3ac8cf0b5724ac
sacret2 <mrbartosiak@gmail.com>
2020-02-18 12:40:45 +0100

    User name token profile authentication via interceptor with callback handler

---
+++ javafirstws/src/main/java/com/bharath/ws/soap/config/UTPasswordCallback.java
@@ -0,0 +1,34 @@
+package com.bharath.ws.soap.config;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.security.auth.callback.Callback;
+import javax.security.auth.callback.CallbackHandler;
+import javax.security.auth.callback.UnsupportedCallbackException;
+
+import org.apache.wss4j.common.ext.WSPasswordCallback;
+
+public class UTPasswordCallback implements CallbackHandler {
+
+	private Map<String, String> passwords = new HashMap<>();
+	
+	public UTPasswordCallback() {
+		passwords.put("marcin", "marcin");
+		passwords.put("cxf", "cxf");
+	}
+	
+	@Override
+	public void handle(Callback[] callbacks) throws IOException, UnsupportedCallbackException {
+		for(Callback callback : callbacks) {
+			WSPasswordCallback passwordCallback = (WSPasswordCallback) callback;
+			String password = passwords.get(passwordCallback.getIdentifier());
+			if(password != null) {
+				passwordCallback.setPassword(password);
+				return;
+			}
+		}
+	}	
+
+}
--- javafirstws/src/main/java/com/bharath/ws/soap/config/WebServiceConfig.java
+++ javafirstws/src/main/java/com/bharath/ws/soap/config/WebServiceConfig.java
@@ -1,9 +1,15 @@
 package com.bharath.ws.soap.config;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.xml.ws.Endpoint;
 
 import org.apache.cxf.Bus;
 import org.apache.cxf.jaxws.EndpointImpl;
+import org.apache.cxf.ws.security.wss4j.WSS4JInInterceptor;
+import org.apache.wss4j.common.ConfigurationConstants;
+import org.apache.wss4j.dom.WSConstants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,8 +24,17 @@ public class WebServiceConfig {
 	
 	@Bean
 	public Endpoint endpoint() {
-		 Endpoint endpoint = new EndpointImpl(bus, new PaymentProcessorImpl());
+		 EndpointImpl endpoint = new EndpointImpl(bus, new PaymentProcessorImpl());
 		 endpoint.publish("/paymentProcessor");
+		 
+		 Map<String, Object> inProps = new HashMap<>();
+		 inProps.put(ConfigurationConstants.ACTION, ConfigurationConstants.USERNAME_TOKEN);
+		 inProps.put(ConfigurationConstants.PASSWORD_TYPE, WSConstants.PW_TEXT);
+		 inProps.put(ConfigurationConstants.PW_CALLBACK_CLASS, UTPasswordCallback.class.getName());
+		 WSS4JInInterceptor inIn = new WSS4JInInterceptor(inProps);
+		 
+		 endpoint.getInInterceptors().add(inIn);
+		 
 		 return endpoint;
 	}
 }
