390f9c5f6a623dda6812e2d66c09c50789375f62
sacret2 <mrbartosiak@gmail.com>
2020-03-01 21:14:28 +0100

    adding and removing stocks

--- StockWatcher/src/com/google/gwt/sample/stockwatcher/client/StockWatcher.java
+++ StockWatcher/src/com/google/gwt/sample/stockwatcher/client/StockWatcher.java
@@ -1,6 +1,14 @@
 package com.google.gwt.sample.stockwatcher.client;
 
+import java.util.ArrayList;
+
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.KeyCodes;
+import com.google.gwt.event.dom.client.KeyDownEvent;
+import com.google.gwt.event.dom.client.KeyDownHandler;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.HorizontalPanel;
@@ -10,38 +18,87 @@ import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
 public class StockWatcher implements EntryPoint {
-  private VerticalPanel mainPanel = new VerticalPanel();
-  private FlexTable stocksFlexTable = new FlexTable();
-  private HorizontalPanel addPanel = new HorizontalPanel();
-  private TextBox newSymbolTextBox = new TextBox();
-  private Button addStockButton = new Button("Add");
-  private Label lastUpdatedLabel = new Label();
+	private VerticalPanel mainPanel = new VerticalPanel();
+	private FlexTable stocksFlexTable = new FlexTable();
+	private HorizontalPanel addPanel = new HorizontalPanel();
+	private TextBox newSymbolTextBox = new TextBox();
+	private Button addStockButton = new Button("Add");
+	private Label lastUpdatedLabel = new Label();
 
-  /**
-   * Entry point method.
-   */
-  public void onModuleLoad() {
-	  // Create table for stock data.
-	    stocksFlexTable.setText(0, 0, "Symbol");
-	    stocksFlexTable.setText(0, 1, "Price");
-	    stocksFlexTable.setText(0, 2, "Change");
-	    stocksFlexTable.setText(0, 3, "Remove");
+	private ArrayList<String> stockSymbols = new ArrayList();
 
-	    addPanel.add(newSymbolTextBox);
-	    addPanel.add(addStockButton);
-	    
-	    mainPanel.add(stocksFlexTable);
-	    mainPanel.add(addPanel);
-	    mainPanel.add(lastUpdatedLabel);
+	/**
+	 * Entry point method.
+	 */
+	public void onModuleLoad() {
+		// Create table for stock data.
+		stocksFlexTable.setText(0, 0, "Symbol");
+		stocksFlexTable.setText(0, 1, "Price");
+		stocksFlexTable.setText(0, 2, "Change");
+		stocksFlexTable.setText(0, 3, "Remove");
 
-	    RootPanel.get("stockList").add(mainPanel);
-	  
-	    newSymbolTextBox.setFocus(true);
-	    
-    // TODO Create table for stock data.
-    // TODO Assemble Add Stock panel.
-    // TODO Assemble Main panel.
-    // TODO Associate the Main panel with the HTML host page.
-    // TODO Move cursor focus to the input box.
-  }
+		addPanel.add(newSymbolTextBox);
+		addPanel.add(addStockButton);
+
+		mainPanel.add(stocksFlexTable);
+		mainPanel.add(addPanel);
+		mainPanel.add(lastUpdatedLabel);
+
+		RootPanel.get("stockList").add(mainPanel);
+
+		// Listen for mouse events on the Add button.
+		addStockButton.addClickHandler(new ClickHandler() {
+			@Override
+			public void onClick(ClickEvent event) {
+				addStock();
+			}
+		});
+
+		newSymbolTextBox.addKeyDownHandler(new KeyDownHandler() {
+
+			@Override
+			public void onKeyDown(KeyDownEvent event) {
+				if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER)
+					addStock();
+			}
+		});
+	}
+
+	/**
+	 * Add stock to FlexTable. Executed when the user clicks the addStockButton or
+	 * presses enter in the newSymbolTextBox.
+	 */
+	private void addStock() {
+		final String symbol = newSymbolTextBox.getText().toUpperCase().trim();
+		newSymbolTextBox.setFocus(true);
+
+		// Stock code must be between 1 and 10 chars that are numbers, letters, or dots.
+		if (!symbol.matches("^[0-9A-Z\\.]{1,10}$")) {
+			Window.alert("'" + symbol + "' is not a valid symbol.");
+			newSymbolTextBox.selectAll();
+			return;
+		}
+
+		if (stockSymbols.contains(symbol))
+			return;
+
+		final int row = stocksFlexTable.getRowCount();
+		stockSymbols.add(symbol);
+		stocksFlexTable.setText(row, 0, symbol);
+
+		Button removeStockBtn = new Button("X");
+
+		removeStockBtn.addClickHandler(new ClickHandler() {
+
+			@Override
+			public void onClick(ClickEvent event) {		
+				int ind = stockSymbols.indexOf(symbol);
+				stockSymbols.remove(ind);
+		        stocksFlexTable.removeRow(ind+1);
+			}
+		});
+
+		this.stocksFlexTable.setWidget(row, 3, removeStockBtn);
+		
+	}
 }
